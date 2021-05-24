9ab183cc784f91b43b3871f82695616550ffd540
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2021-05-24 20:28:57 +0930

    Added some quick 3D stars, must make them span the whole screen later

---
+++ scripts/main.js
@@ -0,0 +1,101 @@
+
+import * as THREE from '/scripts/three.module.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, 6 / 4, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(600, 400);
+camera.position.set(-500, 200, 30);
+
+renderer.render(scene, camera);
+
+
+const cursor = document.getElementById('cursor');
+const window_view = document.getElementById('window');
+const desktop_icons = document.getElementsByClassName('desktop-icon');
+
+let window_clicked = false;
+let offset_x = 0, offset_y = 0;
+
+window_view.style.left = '200px';
+window_view.style.top = '100px';
+
+window_view.addEventListener('mousedown', e => {
+	offset_x = Number(window_view.style.left.replace('px', '')) - e.clientX;
+	offset_y = Number(window_view.style.top.replace('px', '')) - e.clientY;
+
+	window_clicked = true;
+});
+document.addEventListener('mouseup', e => { window_clicked = false; });
+
+document.addEventListener('mousemove', e => {
+	if (window_clicked) {
+		window_view.style.left = Number(e.clientX) + Number(offset_x) + 'px';
+		window_view.style.top = Number(e.clientY) + Number(offset_y) + 'px';
+
+		camera.position.x = (Number(e.clientX) + Number(offset_x) - 700);
+		camera.position.y = -(Number(e.clientY) + Number(offset_y) - 300);
+
+	}
+	cursor.style.left = e.clientX + 'px';
+	cursor.style.top = e.clientY + 'px';
+});
+
+document.addEventListener('mouseenter', e => { cursor.style.display = 'block' });
+document.addEventListener('mouseleave', e => { cursor.style.display = 'none' });
+
+
+document.addEventListener('click', function (e) {
+	e = e || window.event;
+	let target = e.target || e.srcElement,
+		text = target.textContent || target.innerText;
+
+	if (!target.classList.contains('i')) {
+		for (let index = 0; index < desktop_icons.length; index++)
+			document.getElementsByClassName('icon-title')[index].classList.remove("selected");
+	}
+}, false);
+
+
+for (let index = 0; index < desktop_icons.length; index++) {
+	desktop_icons[index].addEventListener('click', e => {
+		for (let index = 0; index < desktop_icons.length; index++)
+			document.getElementsByClassName('icon-title')[index].classList.remove("selected");
+
+		document.getElementsByClassName('icon-title')[index].classList.add("selected");
+	});
+}
+
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+function addStar() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const star = new THREE.Mesh(geometry, material);
+
+	const [x, y] = Array(2).fill().map(() => THREE.MathUtils.randFloatSpread(1000));
+	const z = Array(1).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+	star.position.set(x, y, z);
+	scene.add(star);
+}
+
+Array(2000).fill().forEach(addStar);
+
+
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+}
+
+animate();
